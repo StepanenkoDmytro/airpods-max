5e632009663c830a90894d117ff37d44b8a58c59
Dmytro Stepanenko <dmitriy.s.stepanenko@gmail.com>
2024-02-19 23:16:00 +0200

    components header, navigation choose-color; custom style.css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
+import { ChooseColorComponent } from './components/choose-color/choose-color.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavigationComponent,
+    ChooseColorComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/choose-color/choose-color.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-choose-color',
+  templateUrl: './choose-color.component.html',
+  styleUrls: ['./choose-color.component.css']
+})
+export class ChooseColorComponent {
+
+}
